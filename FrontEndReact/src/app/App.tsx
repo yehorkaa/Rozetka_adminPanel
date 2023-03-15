702cb616349b4747ec0861edb0c5231896ae63f5
Yehor <107763117+yehorkaa@users.noreply.github.com>
2023-03-15 19:16:49 +0200

    data

--- FrontEndReact/src/app/App.tsx
+++ FrontEndReact/src/app/App.tsx
@@ -1,43 +1,26 @@
 import React from "react";
 import LoginForm from "../containers/Login/LoginForm"
 import Products from "../containers/Products/Products";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useNavigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Preview from "../containers/Preview/Preview";
 import PreviewSingle from "../components/PreviewSingle/PreviewSingle";
 import "./App.scss";
+import ErrorPage from "components/ErrorPage/ErrorPage";
+import PrivateRoute from "components/PrivateRoute/PrivateRoute";
 
-interface ProtectedRouteProps {
-  component: React.ComponentType<any>;
-  path: string;
-}
-
-const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ component: Component, path }):any => {
-  const navigate = useNavigate();
-  const isAuthenticated = localStorage.getItem("token");
-  return isAuthenticated ? (
-    <Routes>
-      <Route path={path} element={<Component />} />
-    </Routes>
-  ) : (
-    navigate ("/" , {replace: true})
-  );
-};
 
 const App: React.FC = () => {
   return (
     <Router>
-      <React.Fragment>
-        <ProtectedRoute path="/products" component={Products} />
-        <ProtectedRoute path="/preview" component={Preview} />
-        <ProtectedRoute path="/preview/:id" component={PreviewSingle} />
-      </React.Fragment>
       <Routes>
-        <Route path="/" element={<LoginForm  />} />
+        <Route path="/" element={<LoginForm />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/products" element={<Products/>} />
+          <Route path="/preview" element={<Preview/>} />
+          <Route path="/preview/:id" element={<PreviewSingle />} />
+        </Route>
+
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
   );
